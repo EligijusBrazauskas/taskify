0f8163ec8edac4f26dffaedea0602b938f2a68f8
Eligijus Brazauskas <eligijusbrazauskas@gmail.com>
2024-12-18 22:27:57 +0200

    feat(setup): EB-14: add Biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 
 export const App = () => (
   <div className="h-[400px] w-[400px] bg-slate-50">
     <h1 className="text-3xl">Craftboard Project</h1>
     <Button variant="destructive">Hello</Button>
   </div>
-)
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import './css/index.css'
+import "./css/index.css";
+import { App } from "@/App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import { App } from '@/App'
-
-createRoot(document.getElementById('root')!).render(
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
