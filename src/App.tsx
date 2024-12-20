d8f39109bb873e75d94cb91601be6d1841b4db40
Eligijus Brazauskas <eligijusbrazauskas@gmail.com>
2024-12-20 08:45:14 +0200

    feat(layout): EB-15: add main layout and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { Box } from "@/components/ui/base/box.component";
+import { Flex } from "@/components/ui/base/flex.component";
 import { Button } from "@/components/ui/button";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export const App = () => (
-  <div className="h-[400px] w-[400px] bg-slate-50">
-    <h1 className="text-3xl">Craftboard Project</h1>
-    <Button variant="destructive">Hello</Button>
-  </div>
+  <Flex className="min-h-screen w-screen">
+    <SidebarProvider className=" bg-blue-200">
+      <Sidebar>
+        <SidebarHeader>USER</SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>GROUP</SidebarGroup>
+          <SidebarGroup>GROUP</SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>FOOTER</SidebarFooter>
+      </Sidebar>
+      <Flex className="w-full bg-red-200">
+        <SidebarTrigger />
+      </Flex>
+    </SidebarProvider>
+  </Flex>
 );
