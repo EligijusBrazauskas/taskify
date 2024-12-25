04bdc462e96d5d42654c1ad9152792ad0c1b342d
Eligijus Brazauskas <eligijusbrazauskas@gmail.com>
2024-12-25 11:32:12 +0200

    feat(sidebar): EB-20: remove unneeded type

--- src/types/variants.type.ts
+++
@@ -1,4 +0,0 @@
-export type PartialVariantsProperty<
-  T extends Record<string | "variant", string | number | symbol | null>,
-  K extends keyof T = "variant",
-> = Partial<Record<NonNullable<T[K]>, string>>;
